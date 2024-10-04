6e802c491951a2a215de5d98bce7bcd722c59a7e
elliot sad <bamdadmahdi05@gmail.com>
2024-10-04 23:40:57 +0200

    import and use command  to handle start command

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,18 +1,21 @@
-import { Bot } from "grammy";
-
+import { Bot } from "grammy"; 
+import { handleStart } from './handlers/generalCommands.js'
 import env from "dotenv";
+import connectDB from "../database/db.js";
 env.config();
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
 //send hi there msg to the user and log information
+bot.command('start', handleStart)
+
 bot.on("message", (ctx) => {
   ctx.reply("hi there!");
   console.log(ctx.from);
 });
 
 //start the bot with functionality
-const start = async () => {
+const start = async () => {;
   bot.start();
   console.log("bot running...");
 };
