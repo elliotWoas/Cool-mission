bf9679f4f8d4b0391eaa0cda0697a0b49dd977d4
elliot sad <bamdadmahdi05@gmail.com>
2024-10-05 03:08:46 +0200

    add main menu in menu

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -5,3 +5,15 @@ export const contactRqButton = new Keyboard()
   .add(Keyboard.requestContact("Share Contact"))
   .oneTime()
   .resized();
+
+//mainMenu for all user
+export const mainMenu = new Keyboard()
+  .text("آموزش ها")
+  .text("درباره ما")
+  .text("ماموریت ها ")
+  .text("کد های که نیازه ");
+
+  export default {
+    contactRqButton,
+    mainMenu,
+  }
