54e5990fcb56b4bded48593eb487bdbd8800c5d9
elliot sad <bamdadmahdi05@gmail.com>
2024-10-06 21:20:15 +0200

    handle the start command user

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,5 +1,5 @@
 import { Bot } from "grammy"; 
-import { handleStart } from './handlers/generalCommands.js'
+import { handleStartMessage } from './controllers/messageController.js'
 import env from "dotenv";
 import connectDB from "../database/db.js";
 env.config();
@@ -7,7 +7,7 @@ env.config();
 const bot = new Bot(process.env.BOT_TOKEN);
 
 //send hi there msg to the user and log information
-bot.command('start', handleStart)
+bot.command('start', handleStartMessage)
 
 bot.on("message", (ctx) => {
   ctx.reply("hi there!");
