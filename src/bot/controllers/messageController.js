036e86cccd7cbd3b873f37931bae27b371b3dddc
elliot sad <bamdadmahdi05@gmail.com>
2024-10-06 21:18:48 +0200

    user handler . if user exist return main menu else return new user

--- src/bot/controllers/messageController.js
+++ src/bot/controllers/messageController.js
@@ -1,5 +1,18 @@
+import menu from '../../utils/menu.js';
+import { User } from "../../database/models/User.js"
 
 export const handleStartMessage = async (ctx) => {
     const telegramId = ctx.message.from.id
     
+    // check user in db
+    const user = await User.findOne({ telegramId })
+    try {
+        if(user) {
+            await ctx.reply('سلام خوشگله خوش برگشتی. دوست داری الان چیکار کنیم ؟! ', { reply_markup: menu.mainMenu })
+        }else {
+            ctx.reply('welcome to golestan mehdi first step pls share your contact :):', {reply_markup: menu.contactRqButton})
+        }
+    }catch (err) {
+
+    }
 }
